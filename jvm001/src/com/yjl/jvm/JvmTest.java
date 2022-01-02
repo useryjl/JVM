89c744bffb7ec1630504d329859751b1923d883b
yjl <yjl@1026037885@qq.com>
2022-01-02 14:31:31 +0800

    jvm

---
+++ jvm001/src/com/yjl/jvm/JvmTest.java
@@ -0,0 +1,12 @@
+package com.yjl.jvm;
+
+/**
+ * @author yjl
+ * @Description
+ * @create 2022-01-02
+ */
+public class JvmTest {
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
